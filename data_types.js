0c3b246aed6d5b800b514449ccffe7c478559a5f
OlaBooka <booka@yandex.ru>
2019-01-18 17:18:28 +0300

    convert temperature

--- data_types.js
+++ data_types.js
@@ -5,3 +5,14 @@
 
 console.log(4 * 10 + 5 - 3);
 
+/*
+ * The Celsius-to-Fahrenheit formula:
+ *    F = C x 1.8 + 32
+ * 1. Set the fahrenheit variable to the correct value using the celsius variable and the forumla above
+ * 2. Log the fahrenheit variable to the console
+ */
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit);
