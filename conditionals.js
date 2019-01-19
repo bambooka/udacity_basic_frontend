1306ab73dbc3f61f29b21fa238b02dec6a1f37fd
OlaBooka <booka@yandex.ru>
2019-01-19 11:36:27 +0300

    define which a number - even or odd

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+/*
+ * Write an if...else statement that prints `even` if the
+ * number is even and prints `odd` if the number is odd.
+ */
+
+let number = 2;
+
+if (number % 2 === 0) {
+    console.log('even')
+} else {
+    console.log('odd');
+}
