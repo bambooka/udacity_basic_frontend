0382720905a935092d8acbafcc71dfe8e174e237
OlaBooka <booka@yandex.ru>
2019-01-19 11:52:50 +0300

    using switch statement

--- conditionals.js
+++ conditionals.js
@@ -145,3 +145,38 @@ category = eatsPlants && eatsAnimals ?
             undefined;
 
 console.log(category);
+
+/*Write a switch statement to set the average
+salary of a person based on their type of completed education.*/
+
+let education;
+
+education = "no high school diploma";
+/*education = "a high school diploma";*/
+/*education = "an Associate's degree";*/
+/*education = "a Bachelor's degree";*/
+/*education = "a Master's degree";*/
+/*education = "a Professional degree";*/
+/*education = "a Doctoral degree";*/
+
+let salary;
+
+switch (education) {
+    case "no high school diploma": salary = 25636;
+        break;
+    case "a high school diploma": salary = 35256;
+        break;
+    case "an Associate's degree": salary = 41496;
+        break;
+    case "a Bachelor's degree": salary = 59124;
+        break;
+    case "a Master's degree": salary = 69732;
+        break;
+    case "a Professional degree": salary = 89960;
+        break;
+    case "a Doctoral degree": salary = 84396;
+        break;
+}
+
+console.log('In 2015, a person with ' + education
+    + ' earned an average of ' + '$' + salary.toLocaleString("en-US") + '/year.');
