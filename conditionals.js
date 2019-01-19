7ba933467002502970b967dfcc7d890aaf1e91e1
OlaBooka <booka@yandex.ru>
2019-01-19 11:38:02 +0300

    Print type of group of musician

--- conditionals.js
+++ conditionals.js
@@ -10,3 +10,23 @@ if (number % 2 === 0) {
 } else {
     console.log('odd');
 }
+
+/*
+ * Print type of group of musician by number person in it
+ */
+
+let musicians = 2;
+
+if ((musicians === 0) || (musicians < 0) ) {
+    console.log('not a group');
+} else if (musicians === 1) {
+    console.log('solo');
+} else if (musicians === 2) {
+    console.log('duet');
+} else if (musicians === 3) {
+    console.log('trio');
+} else if (musicians === 4) {
+    console.log('quartet');
+} else if (musicians > 4) {
+    console.log('this is a large group');
+}
